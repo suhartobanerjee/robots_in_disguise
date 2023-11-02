68e374af63d7c5fb75baba9a277768b1db202df9
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-11-02 14:56:34 +0100

    saving the logs in a log folder

--- job_scripts/train_maxcluster.sh
+++ job_scripts/train_maxcluster.sh
@@ -5,12 +5,14 @@
 #$ -l m_mem_free=64G
 #$ -l cuda_name=Tesla-V100-SXM2-32GB
 #$ -l h_rt=24:00:00
+#$ -o /fast/AG_Sanders/suharto/robots_in_disguise/logs/log_$JOB_ID.txt
+#$ -e /fast/AG_Sanders/suharto/robots_in_disguise/logs/log_$JOB_ID.txt
 
 cd /fast/AG_Sanders/suharto/robots_in_disguise/train/
 
 source ~/.bashrc
 conda activate pytorch
-"Conda env activated at : %s" $CONDA_DEFAULT_ENV > log.txt
+printf "Conda env activated at : %s" $CONDA_DEFAULT_ENV
 
 
-python chr21_mock_train.py >> log.txt 2>&1
+python chr21_mock_train.py
