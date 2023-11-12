b9f39ce66fe55f8dd5a6a84716882f2acbd19a3b
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-11-12 01:28:47 +0100

    Some slight changes to notebook.

--- job_scripts/jupyterlab.sh
+++ job_scripts/jupyterlab.sh
@@ -2,14 +2,23 @@
 
 #$ -N jupyterlab
 #$ -l h_rt=48:00:00
-#$ -l m_mem_free=16G
+#$ -l m_mem_free=32G
 #$ -o /fast/AG_Sanders/suharto/robots_in_disguise/.jupyter_logs/jupyterlab_$JOB_ID
 #$ -e /fast/AG_Sanders/suharto/robots_in_disguise/.jupyter_logs/jupyterlab_$JOB_ID
-#$ -cwd
+#$ -wd /fast/AG_Sanders/suharto/robots_in_disguise/
+
+ROOT_DIR=/fast/AG_Sanders/suharto/robots_in_disguise
 
 source ~/.bashrc
 conda activate pytorch
 printf "Conda env activated : %s" $CONDA_DEFAULT_ENV
 
+# moving the previous log to archive and getting back to train folder
+cd $ROOT_DIR/.jupyter_logs/
+
+curr_log="*"$JOB_ID"*"
+find . -maxdepth 1 -type f -not -name $curr_log -exec mv {} archive/. \;
+
+cd $ROOT_DIR
 
 jupyter lab --ip ${HOSTNAME} --port ${SGE_INTERACT_PORT} --no-browser
