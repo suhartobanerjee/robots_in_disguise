ddfaadb4c2e2158f2030ab4f79b50efc8cdad6f2
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2023-12-01 10:50:19 +0100

    removed some pointless files

---
+++ eda/read_h5.R
@@ -0,0 +1,21 @@
+library(rhdf5)
+
+datasets <- H5Fopen("../../proc/6531478_datasets.h5")
+datasets
+
+embed_dim <- datasets$embed_dim
+dim(embed_dim)
+class(embed_dim)
+new_embed_dim <- aperm(embed_dim, c(3,2,1))
+dim(new_embed_dim)
+embed_dim[1,1,1]
+new_embed_dim[1,1,1]
+
+input_tokens <- datasets$input_tokens
+dim(input_tokens)
+input_tokens <- aperm(input_tokens, c(2, 1))
+input_tokens[1,4]
+dim(input_tokens)
+
+
+
